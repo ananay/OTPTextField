a35ddf79b3ae9c102d5d25116d6f7adc57c013ea
ananay <i@ananayarora.com>
2023-08-01 20:07:28 -0700

    Added Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AEOTPTextField",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "AEOTPTextField",
+            targets: ["AEOTPTextField"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AEOTPTextField",
+            dependencies: [],
+            path: "AEOTPTextField/Source"),
+        .testTarget(
+            name: "AEOTPTextFieldTests",
+            dependencies: [
+                "AEOTPTextField",
+            ],
+            path: "AEOTPTextFieldTests")
+    ])
